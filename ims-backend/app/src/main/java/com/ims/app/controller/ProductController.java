936ea7c345575e43b31ac30eb14b87c50c23905f
Aaron Christopher Ramirez <aramirez.4@hotmail.com>
2025-04-22 20:07:51 -0700

    Product Operations
    Create, Delete, Update operations

--- ims-backend/app/src/main/java/com/ims/app/controller/ProductController.java
+++ ims-backend/app/src/main/java/com/ims/app/controller/ProductController.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ims.app.model.Product;
@@ -16,8 +21,33 @@ public class ProductController {
 	@Autowired
 	private ProductSerivce productSerivce;
 
+	// READ ALL request
 	@GetMapping("/products")
 	public List<Product> getAllProducts() {
 		return productSerivce.getAllProducts();
+	}
+
+	// READ request by id
+	@GetMapping("/products/{id}") 
+	public Product getProduct(@PathVariable String id) {
+		return productSerivce.getProduct(id);
+	}
+
+	// POST request
+	@PostMapping("/products")
+	public void addProduct(@RequestBody Product product) {
+		productSerivce.addProduct(product);
+	}
+
+	// UPDATE request
+	@PutMapping("/products/{id}")
+	public void updateProduct(@RequestBody Product product, @PathVariable String id) {
+		productSerivce.updateProduct(id, product);
+	}
+
+	// DELETE request
+	@DeleteMapping("/products/{id}") 
+	public void deleteProduct(@PathVariable String id) {
+		productSerivce.deleteProduct(id);
 	}
 }
--- ims-backend/app/src/main/java/com/ims/app/service/ProductSerivce.java
+++ ims-backend/app/src/main/java/com/ims/app/service/ProductSerivce.java
@@ -9,13 +9,40 @@ import com.ims.app.model.Product;
 @Service
 public class ProductSerivce {
     
-    private List<Product> products = Arrays.asList(
+    private List<Product> products = new ArrayList<>(Arrays.asList(
 			new Product("1", "Soda", 2.25, "Soda Pop", 2),
 			new Product("2", "Red Bull", 3.25, "Energy Drink", 5),
 			new Product("3", "Water", 1.00, "Purified Water", 24)
-		);
+		));
 
+    // READ operation
     public List<Product> getAllProducts(){
         return products;
+    }
+
+    // READ operation by id
+    public Product getProduct(String id){
+        return products.stream().filter(product -> product.getId().equals(id)).findFirst().get();
+    }
+
+    // POST operation
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    // UPDATE operation
+    public void updateProduct(String id, Product product) {
+        for(int i = 0; i < products.size(); i++){
+            Product p = products.get(i);
+            if(p.getId().equals(id)){
+                products.set(i, product);
+                return;
+            }
+        }
+    }
+
+    // DELETE operation
+    public void deleteProduct(String id) {
+        products.removeIf(product -> product.getId().equals(id));
     }
 }
